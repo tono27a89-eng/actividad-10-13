310c058f25995e834520df73900d26601dfb9e0b
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 21:41:00 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_10_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ree un diccionario de notas y calcule el promedio general recorriéndolo con foreach.");
+
+        }
+    }
+}
